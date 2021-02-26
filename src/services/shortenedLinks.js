ab028c9418933fd5749c099159330cf80b592dfa
Alex <kiknalex0@gmail.comLJE-HY6LJE-HY6>
2021-02-26 11:00:24 +0400

    fix

--- src/services/shortenedLinks.js
+++ src/services/shortenedLinks.js
@@ -4,13 +4,13 @@ const proxyUrl = "https://cors-anywhere.herokuapp.com/"
 const baseUrl = "https://api.shrtco.de/v2/shorten?"
 
 const shortenLink = (link) => {
-    const request = axios.get(`${baseUrl}url=${link}`);
-    return request.then(response => response.data, {
+    const request = axios.get(`${baseUrl}url=${link}`, {
         headers: {
-        crossDomain: true,
-        'Access-Control-Allow-Origin': '*'
-        } 
+            crossDomain: true,
+            'Access-Control-Allow-Origin': '*'
+            }
     });
+    return request.then(response => response.data);
 }
 
 export default ({ shortenLink })
